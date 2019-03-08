680da149fec809058d374579cde2570902ffa244
Vinay Bhat <vibha@microsoft.com>
2019-03-08 11:39:30 +0530

    Change

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -11,8 +11,8 @@ namespace CDC.AzureEventCollector
         [Fact]
         public void Test1()
         {
-            var eventHubsConf = new EventHubsConfiguration("Endpoint=sb://cdctentacles.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Cbnmd50ea7iozxa5NAcqlF4727NGENguVNWyPNl7CsI=",	
-            "cluster1", "azureeventhubscntr", "eventhubsstorageacnt", "8gSa/MZK709NUuYcoO9Uv4y/odE8egTIbEmXsO6+54OHP/Sp3ZtCfsIsBjuMqZ6DjBLYXsK1YDVmlXCKB94Kjg==");
+            var eventHubsConf = new EventHubsConfiguration("<hubskey>",	
+            "cluster1", "azureeventhubscntr", "eventhubsstorageacnt", "<key>");
             var collectorEventsProducerFactory = new CollectorEventsProducerFactory(eventHubsConf);
             var collector = new TestEventCollector();
             var eventSource = collectorEventsProducerFactory.CreateSource(collector, new TestHealthStore());
